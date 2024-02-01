815c4428566ab3ce1907cf90828acc04fc4a8acf
Jari Bou <bourdietome@gmail.com>
2024-02-02 00:46:52 +0100

    Changelog update and moved IOHelper.cs

--- Assets/GraphicsLabor/Scripts/Editor/Windows/ScriptableObjectCreatorWindow.cs
+++ Assets/GraphicsLabor/Scripts/Editor/Windows/ScriptableObjectCreatorWindow.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection;
 using GraphicsLabor.Scripts.Attributes.LaborerAttributes.ScriptableObjectAttributes;
 using GraphicsLabor.Scripts.Core.Utility;
+using GraphicsLabor.Scripts.Editor.Utility;
 using GraphicsLabor.Scripts.Editor.Utility.GUI;
 using UnityEditor;
 using UnityEngine;
