701baa2d239f51e25a3cca68c10ffd8491197753
Jari Bou <bourdietome@gmail.com>
2024-02-02 00:42:41 +0100

    Removed property editing

--- Assets/GraphicsLabor/Samples/SampleScripts/AttributesTestScript.cs
+++ Assets/GraphicsLabor/Samples/SampleScripts/AttributesTestScript.cs
@@ -22,10 +22,10 @@ namespace GraphicsLabor.Samples.SampleScripts
 
         [SerializeField, HorizontalSeparator(5, LaborColor.Blue), ReadOnly]
         private string _uneditable = "You can only read this";
-        [SerializeField] private bool _enableEditingOfProp3;
+        [SerializeField] private bool _showProp3;
         [ShowProperty] public int AutoProp1 { get; private set; }
-        [ShowProperty(true), Label("Editable Prop")] public int AutoProp2 { get; private set; }
-        [ShowProperty, EnableIf(nameof(_enableEditingOfProp3))] public int AutoProp3 { get; private set; }
+        [ShowProperty, Label("Shown Prop")] public int AutoProp2 { get; private set; }
+        [ShowProperty, ShowIf(nameof(_showProp3))] public int AutoProp3 { get; private set; }
 
         [SerializeField, HorizontalSeparator, TabProperty("")]
         private GameObject _invalidTab;
